c5d38c44f7c1fd787d3d32b8e74343c6641c842f
KyselovYurii <kyselov.yurii@gmail.com>
2022-04-03 12:04:54 +0300

    Task2

--- FeedManager/FeedManager.Task2/Importers/Delta1FeedImporter.cs
+++ FeedManager/FeedManager.Task2/Importers/Delta1FeedImporter.cs
@@ -1,14 +1,14 @@
-﻿using FeedManager.Task2.Database;
+﻿using System.Collections.Generic;
+using FeedManager.Task1.FeedImporters;
+using FeedManager.Task2.Database;
 using FeedManager.Task2.Feeds;
-using System;
-using System.Collections.Generic;
+using FeedManager.Task2.Matchers;
 
 namespace FeedManager.Task2.Importers
 {
     public class Delta1FeedImporter
     {
         private readonly IDatabaseRepository database;
-
         public Delta1FeedImporter(IDatabaseRepository database)
         {
             this.database = database;
@@ -16,7 +16,24 @@ namespace FeedManager.Task2.Importers
 
         public void Import(IEnumerable<Delta1Feed> feeds)
         {
-            throw new NotImplementedException();                    
+            var matcher = new Delta1FeedMatcher();
+            var validator = new Delta1FeedValidator();
+            var existingFeeds = database.LoadFeeds<Delta1Feed>();
+            foreach (var feed in feeds)
+            {
+                if (!existingFeeds.Exists(f => matcher.Match(feed, f)))
+                {
+                    var validateResult = validator.Validate(feed);
+                    if (validateResult.IsValid)
+                    {
+                        database.SaveFeed(feed);
+                    }
+                    else
+                    {
+                        database.SaveErrors(feed.StagingId, validateResult.Errors);
+                    }
+                }
+            }
         }
     }
 }
--- FeedManager/FeedManager.Task2/Importers/EmFeedImporter.cs
+++ FeedManager/FeedManager.Task2/Importers/EmFeedImporter.cs
@@ -1,14 +1,14 @@
-﻿using FeedManager.Task2.Database;
+﻿using System.Collections.Generic;
+using FeedManager.Task1.FeedImporters;
+using FeedManager.Task2.Database;
 using FeedManager.Task2.Feeds;
-using System;
-using System.Collections.Generic;
+using FeedManager.Task2.Matchers;
 
 namespace FeedManager.Task2.Importers
 {
     public class EmFeedImporter
     {
         private readonly IDatabaseRepository database;
-
         public EmFeedImporter(IDatabaseRepository database)
         {
             this.database = database;
@@ -16,7 +16,24 @@ namespace FeedManager.Task2.Importers
 
         public void Import(IEnumerable<EmFeed> feeds)
         {
-            throw new NotImplementedException();
+            var matcher = new EmFeedMatcher();
+            var validator = new EmFeedValidator();
+            var existingFeeds = database.LoadFeeds<EmFeed>();
+            foreach (var feed in feeds)
+            {
+                if (!existingFeeds.Exists(f => matcher.Match(feed, f)))
+                {
+                    var validateResult = validator.Validate(feed);
+                    if (validateResult.IsValid)
+                    {
+                        database.SaveFeed(feed);
+                    }
+                    else
+                    {
+                        database.SaveErrors(feed.StagingId, validateResult.Errors);
+                    }
+                }
+            }
         }
     }
 }
